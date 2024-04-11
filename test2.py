6e443f11f76296ec4944ae6ebf774b4506695356
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-04-11 18:22:46 -0300

    instalei pycharm

--- test2.py
+++
@@ -1,8 +0,0 @@
-voto = (input('Digite 1 ou 2: '))
-
-if int(voto) == 1:
-    print('Você escolheu o 1.')
-elif int(voto) == 2:
-    print('Você escolheu o 2.')
-else:
-    print('Escolha um número válido.')
