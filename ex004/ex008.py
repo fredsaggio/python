60dd6924514ace7da8d3e4d3011c42d1d6392613
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-04-29 20:14:34 -0300

    Revisando exercício

--- ex004/ex008.py
+++ ex004/ex008.py
@@ -9,23 +9,28 @@ i = 1
 total = 0
 valor = None
 
+while True:
+    valor = float(input(f'Digite o valor do {i}º produto: '))
+    produto = f'{i}º produto: R${valor:.2f}'
 
-while valor != 0:
-    valor = float(input(f'Digite o valor do produto {i}'))
-    produto = f'Produto {i}: R${valor:.2f}'
+    if valor == 0:
+        dinheiro = float(input('Com quanto você quer pagar?'))
+        troco = dinheiro - total
+        
+        if dinheiro < total:
+            print('Você não tem dinheiro o suficiente')
+        else:
+            print(f'Seu troco é de: R${troco:.2f}')
+            i = 1
+
+    if valor == -1:
+        print('Programa finalizado')
+        break
     
     total += valor
     print(produto)
     print(f'R${total:.2f}')
     i += 1
-
-dinheiro = float(input('Com quanto você quer pagar?'))
-troco = dinheiro - total
-
-if dinheiro < total:
-    print('Você não tem dinheiro o suficiente')
-else:
-    print(f'Seu troco é de: R${troco:.2f}')
 
 
 
--- test.py
+++ test.py
@@ -1,21 +1,3 @@
-while True:
-    i = 0          
-    flag_num = False
-    flag_str = False
-    senha = str(input('Digite uma senha: '))
+a = 'abcd'
 
-    if 8 <= len(senha) <= 16:
-        for i in range(len(senha)):
-
-            if senha[i].isnumeric():
-                flag_num = True
-            else:
-                flag_str = True
-
-        if flag_num and flag_str:
-            print('Senha válida!')
-            break
-        else: 
-            print('Senha inválida! Pelo menos uma letra e um número!')
-    else:
-        print('No mínimo 8 caracteres e no máximo 16!')
+print(a[-1: -5: -1])
