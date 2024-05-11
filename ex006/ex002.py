6d16e5ec70793d747c18fee03541fefbd66776ec
Frederico Santos Saggio <150391419+fredsaggio@users.noreply.github.com>
2024-05-11 18:36:37 -0300

    Exercícios de lista

---
+++ ex006/ex002.py
@@ -0,0 +1,9 @@
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+listaVazia = []
+sublista = []
+num = int(input('Digite um número: '))
+
+
+while True:
+    for i in range(num+1):
+        sublista.append(lista[i])
